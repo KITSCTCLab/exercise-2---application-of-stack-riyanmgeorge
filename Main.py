d8c443d07ff396b8f6ec5c08f560e18c172e6611
riyanmgeorge <110010573+riyanmgeorge@users.noreply.github.com>
2022-07-28 21:32:42 +0530

    Initial

--- Main.py
+++ Main.py
@@ -74,4 +74,4 @@ evaluate = Evaluate(len(tokens))
 if evaluate.validate_postfix_expression(tokens):
     print(evaluate.evaluate_postfix_expression(tokens))
 else:
-    print('Invalid postfix expression')
+    print('Invalid postfix Statement')
